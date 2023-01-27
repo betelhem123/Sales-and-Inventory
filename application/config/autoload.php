eed9523142baeb7dcd0e235dc83f4b9681886918
betelhem123 <96373734+betelhem123@users.noreply.github.com>
2023-01-26 20:27:49 -0800

    first commit

---
+++ application/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$autoload['packages'] = array();
+
+$autoload['libraries'] = array('database','session','layout','user_agent','form_validation');
+
+$autoload['drivers'] = array();
+
+$autoload['helper'] = array('url','file','util');
+
+
+$autoload['config'] = array();
+
+
+$autoload['language'] = array();
+
+
+$autoload['model'] = array();
